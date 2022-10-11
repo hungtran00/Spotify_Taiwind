acc32d41b4f01c5d2b9acd810c61bce52f8adbb9
tranhung1705 <85281469+tranhung1705@users.noreply.github.com>
2022-10-11 20:11:20 +0700

    ..

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import './App.css';
+import { Songs } from './components/Context';
 import DetailSong from './components/DetailSong';
 import Header from './components/Header';
 import ListSong from './components/ListSong';
+import DataSong from './data/songs.json'
 
 function App() {
   return (
     <div className="">
-        <Header/>
-        <div className='grid grid-cols-3 h-screen-header-player bg-slate-700'>
-            <DetailSong/>
-            <ListSong/>
-        </div>
+        <Songs.Provider value={{DataSong}}>
+          <Header/>
+          <div className='grid grid-cols-3 h-screen-header-player bg-slate-700'>
+              <DetailSong/>
+              <ListSong/>
+          </div>
+        </Songs.Provider>
     </div>
   );
 }
--- src/components/ListSong.js
+++ src/components/ListSong.js
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import {IoMdDownload} from 'react-icons/io'
+import { Songs } from './Context';
 
 function ListSong(props) {
+    const DataSongs = useContext(Songs)
     return (
         <div className='col-span-2'>
-            2
+            <table className='table-auto w-full'>
+                <thead className='text-white h-12'>
+                    <tr>
+                        <th className='w-[10%]'>#</th>
+                        <th className='text-left'>Title</th>
+                        <th className='w-[10%]'>Author</th>
+                        <th className='w-[10%]'><IoMdDownload/></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {DataSongs.map((song, index) => (
+                        <tr key={index}>
+                            <td className='text-center'>{song.id}</td>
+                            <td className='text-center'>{song.name}</td>
+                            <td className='text-center'>{song.author}</td>
+                            <td className='text-center'>
+                                <a href={song.url}>
+                                    <IoMdDownload/>
+                                </a>
+                            </td>
+                        </tr>
+                        ))}
+                </tbody>
+            </table>
         </div>
     );
 }
