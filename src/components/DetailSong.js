e1474f81452536ce0dd3bda454140126c6c404e4
tranhung1705 <85281469+tranhung1705@users.noreply.github.com>
2022-10-13 23:36:38 +0700

    ..

--- src/components/DetailSong.js
+++ src/components/DetailSong.js
@@ -1,16 +1,18 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Songs } from '../Context';
 
 function DetailSong(props) {
+    const {song} = useContext(Songs)
     return (
         <div className='col-span-1 p-3'>
             <h2 className='text-cyan-700 font-bold pb-2'>Now Playing</h2>
-            <p className=' text-xl text-gray-400'>Sing me to leep</p>
+            <p className=' text-xl text-gray-400'>{song.name}</p>
             <div className='w-[240px] m-auto mt-[30px]'>
-                <img className='' src="https://i.scdn.co/image/ab6761610000e5ebc02d416c309a68b04dc94576" alt="avatar" />
+                <img className='' src={song.links.images[0].url} alt="avatar" />
             </div>
             <div className='flex mt-[30px] justify-evenly items-center mx-4'>
-                <img className='w-[70px] rounded-full' src="https://i.scdn.co/image/ab6761610000e5ebc02d416c309a68b04dc94576" alt="avatar" />
-                <span className='text-xl text-white'>Alan Walker</span>
+                <img className='w-[70px] rounded-full' src={song.links.images[1].url} alt="avatar" />
+                <span className='text-xl text-white'>{song.name}</span>
             </div>
         </div>
     );
